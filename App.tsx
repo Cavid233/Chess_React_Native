41728e96c8a55606211b7067ecec766802df429b
Cavid233 <salmanov.cavid1@gmail.com>
2023-10-01 20:26:34 +0400

    moves are ready

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const screenWidth = Dimensions.get('window').width;
@@ -28,11 +28,44 @@ class Pawn extends Figures {
   constructor(x: number, y: number, color: string) {
     super('chess-pawn', x, y, color);
   }
+
+  checkRules(destX: number, destY: number) {
+
+    if (this.color === 'white') {
+      console.log(this.y - destY === 1);
+      if (
+        this.y === destY &&
+        this.x === 6 &&
+        (this.x - destX === 1 || this.x - destX === 2)
+      ) {
+        return true;
+      } else if (this.x - destX === 1 && this.y === destY) {
+        return true;
+      }
+    } else if (this.color === 'black') {
+      if (
+        this.y === destY &&
+        this.x === 1 &&
+        (destX - this.x === 1 || destX - this.x === 2)
+      ) {
+        return true;
+      } else if (destX - this.x === 1 && this.y === destY) {
+        return true;
+      }
+    }
+  }
 }
 
 class Rook extends Figures {
   constructor(x: number, y: number, color: string) {
     super('chess-rook', x, y, color);
+  }
+
+  checkRules(destX: number, destY: number) {
+    if (destX === this.x || destY === this.y) {
+      return true;
+    }
+    return false;
   }
 }
 
@@ -40,11 +73,26 @@ class Knight extends Figures {
   constructor(x: number, y: number, color: string) {
     super('chess-knight', x, y, color);
   }
+
+  checkRules(destX: number, destY: number) {
+    // Check if the move follows the L-shaped pattern of a knight.
+    const deltaX = Math.abs(destX - this.x);
+    const deltaY = Math.abs(destY - this.y);
+
+    return (deltaX === 1 && deltaY === 2) || (deltaX === 2 && deltaY === 1);
+  }
 }
 
 class Bishop extends Figures {
   constructor(x: number, y: number, color: string) {
     super('chess-bishop', x, y, color);
+  }
+
+  checkRules(destX: number, destY: number) {
+    const checkX = Math.abs(destX - this.x);
+    const checkY = Math.abs(destY - this.y);
+
+    return checkX === checkY;
   }
 }
 
@@ -52,11 +100,32 @@ class Queen extends Figures {
   constructor(x: number, y: number, color: string) {
     super('chess-queen', x, y, color);
   }
+
+  checkRules(destX: number, destY: number) {
+    if (
+      Bishop.prototype.checkRules.bind(this)(destX, destY) ||
+      Rook.prototype.checkRules.bind(this)(destX, destY)
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
 
 class King extends Figures {
   constructor(x: number, y: number, color: string) {
     super('chess-king', x, y, color);
+  }
+
+  checkRules(destX: number, destY: number) {
+    const deltaX = Math.abs(destX - this.x);
+    const deltaY = Math.abs(destY - this.y);
+
+    return (
+      (deltaX === 1 && deltaY === 0) ||
+      (deltaX === 0 && deltaY === 1) ||
+      (deltaX === 1 && deltaY === 1)
+    );
   }
 }
 
@@ -73,7 +142,7 @@ const initialChessboard = [
     new Rook(0, 7, 'black'),
   ],
   // Rank 2 (pawns in the second row)
-  [...Array.from({ length: 8 }, (_, colIndex) => new Pawn(1, colIndex, 'black'))],
+  [...Array.from({length: 8}, (_, colIndex) => new Pawn(1, colIndex, 'black'))],
   // Rank 3 (empty)
   [null, null, null, null, null, null, null, null],
   // Rank 4 (empty)
@@ -83,7 +152,7 @@ const initialChessboard = [
   // Rank 6 (empty)
   [null, null, null, null, null, null, null, null],
   // Rank 7 (pawns in the seventh row)
-  [...Array.from({ length: 8 }, (_, colIndex) => new Pawn(6, colIndex, 'white'))],
+  [...Array.from({length: 8}, (_, colIndex) => new Pawn(6, colIndex, 'white'))],
 
   // Rank 8 (the eighth row from Black's perspective)
   [
@@ -100,35 +169,43 @@ const initialChessboard = [
 
 let move = 0;
 let selectedFigure: any;
-let moveOrder = "white"
 
 const App = () => {
-
   const [chessBoard, setChessBoard] = useState(initialChessboard);
+  const [moveOrder, setMoveOrder] = useState('white');
 
   const figureMoveHandler = (rowIndex: number, colIndex: number) => {
-
     if (move === 0) {
-      console.log("0")
+      console.log('0');
       selectedFigure = initialChessboard[rowIndex][colIndex];
-      console.log("selectedFigure", selectedFigure);
+      console.log('selectedFigure', selectedFigure);
 
       if (selectedFigure && moveOrder === selectedFigure.color) {
-          move++
+        move++;
       }
-
     } else {
-      console.log('1')
+      console.log('1');
       const newChessBoard = [...chessBoard];
 
       const selectedDestination = newChessBoard[rowIndex][colIndex];
-      if(selectedDestination === null || selectedDestination.color !== selectedFigure.color){
+      if (
+        selectedDestination === null ||
+        selectedDestination.color !== selectedFigure.color
+      ) {
+        // if (selectedFigure.name === "chess-bishop" || selectedFigure.name === "chess-rook" || selectedFigure.name === "chess-queen") {
+        // console.log("hey")
+        const isValid = selectedFigure.checkRules(rowIndex, colIndex);
+        if (!isValid) {
+          return;
+        }
+        // }
+
         newChessBoard[rowIndex][colIndex] = selectedFigure;
         newChessBoard[selectedFigure.x][selectedFigure.y] = null;
         setChessBoard(() => newChessBoard);
         selectedFigure.x = rowIndex;
         selectedFigure.y = colIndex;
-        moveOrder = moveOrder === "white" ? "black" : "white"
+        setMoveOrder(moveOrder === 'white' ? 'black' : 'white');
       }
 
       move = 0;
@@ -137,9 +214,10 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+
+      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
         {chessBoard.map((row, rowIndex) => (
-          <View key={rowIndex} style={{ flexDirection: 'row' }}>
+          <View key={rowIndex} style={{flexDirection: 'row'}}>
             {row.map((square, colIndex) => (
               <TouchableWithoutFeedback
                 onPress={() => figureMoveHandler(rowIndex, colIndex)}
@@ -163,6 +241,7 @@ const App = () => {
           </View>
         ))}
       </View>
+
     </View>
   );
 };
@@ -182,21 +261,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-
-{
-  /* {Array.from({ length: 8 }, (_, rowIndex) => (
-          <View key={rowIndex} style={{ flexDirection: 'row' }}>
-            {Array.from({ length: 8 }, (_, colIndex) => (
-              <View
-                key={colIndex.toString()}
-                style={{
-                  ...styles.card,
-                  backgroundColor:
-                    (rowIndex + colIndex) % 2 === 0 ? '#6c523b' : '#90826d',
-                }}>
-
-              </View>
-            ))}
-          </View>
-        ))} */
-}
